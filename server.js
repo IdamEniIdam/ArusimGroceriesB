231869c66932dd9cbafe177cb9a838ddb26ef646
IdamEniIdam <idameni89@gmail.com>
2020-11-12 16:12:17 -0500

    Server file edied

--- server.js
+++ server.js
@@ -18,12 +18,7 @@ app.use(bodyParser.json());
 
 app.use(cors()); // Use this after the variable declaration
 
-app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 // DB Config
